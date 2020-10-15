efc29bada8bf3998c1f56512d23b88ccb192c60f
Oleg Sedinkin <a@aw.wtf>
2020-10-15 21:13:47 +0500

    fix ssh port

--- src/tune/ssh.sh
+++ src/tune/ssh.sh
@@ -1,14 +1,14 @@
 function tuneSSH() {
-  if [ -z "$ssh_port" ]; then
-    read -p "SSH port [22]: " ssh_port
+  if [ -z "$ssh" ]; then
+    read -p "SSH port [22]: " ssh
 
-    if [ -z "$ssh_port" ]; then
-      ssh_port=22
+    if [ -z "$ssh" ]; then
+      ssh=22
     fi
   fi
 
   printProcess "Update SSH"
-  sed -i -r -e "s/^(\#?)(Port)([[:space:]]+).+$/\2\3$ssh_port/" /etc/ssh/sshd_config
+  sed -i -r -e "s/^(\#?)(Port)([[:space:]]+).+$/\2\3$ssh/" /etc/ssh/sshd_config
   sed -i -r -e "s/^(\#?)(PermitRootLogin)([[:space:]]+).+$/\2\3no/" /etc/ssh/sshd_config
 
   if [ $1 ]; then
@@ -19,7 +19,7 @@ function tuneSSH() {
   printProcessSuccess
 
   printProcess "Enable UFW"
-  ufw allow $ssh_port > /dev/null 2>&1
-  ufw --force enable > /dev/null 2>&1
+  ufw allow $ssh_port >/dev/null 2>&1
+  ufw --force enable >/dev/null 2>&1
   printProcessSuccess
 }
