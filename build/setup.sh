740b2b67112efc72b4ba27fbcda76cdc57b9ac55
Oleg Sedinkin <a@aw.wtf>
2023-10-19 07:17:17 +0500

    add deploy user

--- build/setup.sh
+++ build/setup.sh
@@ -75,42 +75,65 @@ function tuneSSH() {
   ufw --force enable >/dev/null 2>&1
   printProcessSuccess
 }
-function tuneUser() {
-  if [ -z "$user" ]; then
-    read -p "User name: " user
+function createUser(){
+    printProcess "Create new user $1"
 
-    if [ -z "$user" ]; then
-      echo "User name required"
-      exit 3
-    fi
-  fi
+    password=$(openssl rand -base64 12)
+    encryptedPassword=$(perl -e 'print crypt($ARGV[0], "password")' "$password")
 
-  egrep "^$user" /etc/passwd >/dev/null
-
-  if [ $? -eq 0 ]; then
-    echo "User $user already exists"
-    exit 4
-  fi
-
-  password=$(openssl rand -base64 12)
-  encryptedPassword=$(perl -e 'print crypt($ARGV[0], "password")' "$password")
-
-  printProcess "Create new sudo user $user"
-  useradd -d "/home/$user" -m -p "$encryptedPassword" -s "/bin/bash" $user
-  usermod -aG sudo $user
-  printProcessSuccess
-
-  printf "password \e[38;5;255;48;5;237m %s \e[0m\n" "$password"
-
-  if [ -f /root/.ssh/authorized_keys ]; then
-    authorized_keys=true
-    printProcess "Copy root's authorized keys to new user "
-
-    mkdir -p /home/$user/.ssh
-    cp /root/.ssh/authorized_keys /home/$user/.ssh/authorized_keys
-    chown -R $user:$user /home/$user/.ssh
+    useradd -d "/home/$1" -m -p "$encryptedPassword" -s "/bin/bash" $1
+    usermod -aG sudo $1
+    usermod -aG docker $1
+    usermod -aG app $1
 
     printProcessSuccess
+    printf "password \e[38;5;255;48;5;237m %s \e[0m\n" "$password"
+}
+
+function addUser() {
+  egrep "^$1" /etc/passwd >/dev/null
+
+  if [ $? -eq 0 ]; then
+    printProcess "User $1 already exists"
+    printProcessFail
+  else
+    createUser $1
+
+    if [ -f /root/.ssh/authorized_keys ]; then
+      authorized_keys=true
+      printProcess "Copy root's authorized keys to new user "
+
+      mkdir -p /home/$1/.ssh
+      cp /root/.ssh/authorized_keys /home/$1/.ssh/authorized_keys
+      chown -R $1:$1 /home/$1/.ssh
+
+      printProcessSuccess
+    fi
+  fi
+}
+
+function addDeployUser() {
+  egrep "^$1" /etc/passwd >/dev/null
+
+  if [ $? -eq 0 ]; then
+    printProcess "User $1 already exists"
+    printProcessFail
+  else
+    createUser $1
+
+    password=$(openssl rand -base64 12)
+    ssh-keygen -f key -t ed25519 -N "$password" -C "$1" &>/dev/null
+
+    mkdir -p /home/$1/.ssh
+    cat key.pub > /home/$1/.ssh/authorized_keys
+    chown -R $1:$1 /home/$1/.ssh
+
+    printf "%s key passphrase \e[38;5;255;48;5;237m %s \e[0m\n" "$1" "$password"
+    printf "%s private key:\n" "$1"
+    cat key
+    printf "\n"
+
+    rm key*
   fi
 }
 printLogo
@@ -124,7 +147,19 @@ fi
 
 authorized_key_exists=false
 
-tuneUser
+groupadd docker
+groupadd app
+
+mkdir /srv/app
+chgrp app /srv/app
+
+if [ -z "$user" ]; then
+  addUser $user
+fi
+
+if [ -z "$deploy" ]; then
+  addDeployUser $deploy
+fi
 
 if [ $authorized_key_exists ]; then
   tuneSSH true
--- src/setup.sh
+++ src/setup.sh
@@ -9,7 +9,19 @@ fi
 
 authorized_key_exists=false
 
-tuneUser
+groupadd docker
+groupadd app
+
+mkdir /srv/app
+chgrp app /srv/app
+
+if [ -z "$user" ]; then
+  addUser $user
+fi
+
+if [ -z "$deploy" ]; then
+  addDeployUser $deploy
+fi
 
 if [ $authorized_key_exists ]; then
   tuneSSH true
